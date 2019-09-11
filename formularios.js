941f8ce7df41bd844bf3152933c2bdb82f0b171e
fbarreraCENTRO <55198090+fbarreraCENTRO@users.noreply.github.com>
2019-09-11 09:29:04 -0500

    Create formularios.js

---
+++ formularios.js
@@ -0,0 +1,7 @@
+(function(i,s,o,g,r,a,m){i[â€˜GoogleAnalyticsObjectâ€™]=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,â€™scriptâ€™,â€™//www.google-analytics.com/analytics.jsâ€™,â€™gaâ€™);
+
+ga(â€˜createâ€™, â€˜UA-109517718-1â€˜, â€˜autoâ€™);
+ga(â€˜sendâ€™, â€˜pageviewâ€™);
