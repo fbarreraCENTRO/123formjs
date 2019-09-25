cd5282a042c0f02b2ac71ff16c15b22ac5ac842e
Fernando Barrera <fbarrera@centro.edu.mx>
2019-09-25 11:35:07 -0500

    Update formularios.js

--- formularios.js
+++ formularios.js
@@ -1,20 +1,6 @@
-/*field ids*/
-var phone_cid = '57222769';
-
-/* Form ids */
-var numeric_validation_form_id = "4923015";
-
 $(document).ready(function (e) {
 
    alert("funciona :D");
 
 });
 
-
-function phone_number_actions(){
-    var keycode = event.which;
-    if (!(event.shiftKey == false && (keycode == 46 || keycode == 8 || keycode == 37 || keycode == 39 || (keycode >= 48 && keycode <= 57)))) {
-        alert("Please insert only numeric values.");
-        event.preventDefault();
-    }
-}
