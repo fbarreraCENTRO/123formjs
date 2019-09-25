d368c460ecef3bf64207e3c2790827c4a513e53c
Fernando Barrera <fbarrera@centro.edu.mx>
2019-09-25 12:04:01 -0500

    Update formularios.js

--- formularios.js
+++ formularios.js
@@ -1,7 +1,5 @@
-var s = document.createElement("script");
-s.src = "http://code.jquery.com/jquery-1.11.0.min.js";
-s.onload = function(e){ 
-    console.log( "ready!" );
-    alert("funciona :D");
- }; 
-document.head.appendChild(s); 
+$(document).ready(function (e) {
+
+    alert("Hola prro");
+
+});
